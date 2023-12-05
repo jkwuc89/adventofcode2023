8e4fc66192e994be1cfe2f36f44191693acff6bc
Keith Wedinger <keith@emporatitle.com>
2023-12-05 16:22:35 -0500

    Complete day 2 part 2

--- lib/day2.rb
+++ lib/day2.rb
@@ -13,7 +13,14 @@ class Day2
     possible_game_ids.compact.sum
   end
 
-  def part2(game_input:); end
+  def part2(game_input:)
+    color_powers = game_input.map do |game_input_line|
+      minimum_cubes_per_color = minimum_cubes_per_color(game_input_line)
+      minimum_cubes_per_color[:red] * minimum_cubes_per_color[:green] * minimum_cubes_per_color[:blue]
+    end
+
+    color_powers.sum
+  end
 
   private
 
@@ -37,5 +44,19 @@ class Day2
         end
       end
     end
+  end
+
+  def minimum_cubes_per_color(game_input_line)
+    all_cubes_pulled = game_input_line.split(':').last.split(';').join
+
+    min_red_cubes_needed = all_cubes_pulled.scan(/(\d+) red/).flatten.map(&:to_i).max
+    min_green_cubes_needed = all_cubes_pulled.scan(/(\d+) green/).flatten.map(&:to_i).max
+    min_blue_cubes_needed = all_cubes_pulled.scan(/(\d+) blue/).flatten.map(&:to_i).max
+
+    {
+      red: min_red_cubes_needed,
+      green: min_green_cubes_needed,
+      blue: min_blue_cubes_needed
+    }
   end
 end
